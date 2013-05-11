68f71d382084bab3240ce475e8624cccca99b228
adamnew123456 <adamnew123456@gmail.com>
2013-05-11 00:28:04 -0400

    Added UDP syntax support to the CLI proxy tool and the DBus interface.

--- proxy-service-dbus.py
+++ proxy-service-dbus.py
@@ -1,9 +1,14 @@
+
 """
 The service component for the port redirector.
 
 The service runs over a DBus service, accepts requests to add, remove,
 and modify port redirects.
 """
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('[' + __name__ + ']')
 
 #
 # For the interface to the service.
@@ -24,44 +29,47 @@ import socket
 
 gobject.threads_init()
 
+def protocol_string_to_enum(portspec):
+    return (portspec[0], portspec[1], portforward.Protocol.FromString[portspec[2]])
+
 class PortRedirector(dbus.service.Object):
     def __init__(self):
         bus = dbus.service.BusName('org.new123456.Proxy', bus = dbus.SessionBus())
         dbus.service.Object.__init__(self, bus, '/org/new123456/Proxy')
 
     @dbus.service.method('org.new123456.Proxy',
-                         in_signature = '(si)(si)',
+                         in_signature = '(sis)(sis)',
                          out_signature='b')
     def AddMapping(self, src, dest):
         try:
-            portforward.add_mapping(src, dest)
-            print "Done"
+            portforward.add_mapping(protocol_string_to_enum(src), protocol_string_to_enum(dest))
+            logger.debug("Done")
             return True
-        except socket.error:
-            print "Fail"
+        except socket.error as e:
+            logger.debug("Fail\n\t-%s", e)
             return False
 
     @dbus.service.method('org.new123456.Proxy',
-                         in_signature='(si)',
+                         in_signature='(sis)',
                          out_signature='b')
     def RemoveMapping(self, src):
         try:
-            portforward.del_mapping(src)
-            print "Done"
+            portforward.del_mapping(protocol_string_to_enum(src))
+            logger.debug("Done")
             return True
         except KeyError:
-            print "Fail"
+            logger.debug("Fail")
             return False
 
     @dbus.service.method('org.new123456.Proxy',
                          in_signature='',
-                         out_signature='a(sisi)')
+                         out_signature='a(sissis)')
     def ReadMappings(self):
         src_to_dest = []
         for src in portforward.src_to_svr:
             svr = portforward.src_to_svr[src]
             dest = portforward.svr_to_dest[svr.fileno()]
-            src_to_dest.append((src[0], src[1], dest[1], dest[2]))
+            src_to_dest.append((src[0], src[1], portforward.Protocol.ToString[src[2]], dest[1], dest[2], portforward.Protocol.ToString[dest[3][1]]))
         return src_to_dest
 
 DBusGMainLoop(set_as_default=True)
